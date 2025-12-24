2cfed5ff889b268f6fb2c9edfaeb3f7c6ecc350d
Vik Borges <v1k@protonmail.com>
2025-12-24 11:42:21 +0000

    🚀

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import index from "./index.html";
+import clickyButtons from "./clicky-buttons.html";
+
+Bun.serve({
+  routes: {
+    "/": index,
+    "/clicky-buttons": clickyButtons,
+  },
+});
+
+console.log("Design Candies running at http://localhost:3000");
