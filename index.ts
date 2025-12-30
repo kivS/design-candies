6bcf6ce1a450b7e7d861a8b4bf779780e578d0a2
Vik Borges <v1k@protonmail.com>
2025-12-30 13:48:52 +0000

    🚀

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import index from "./index.html";
 import clickyButtons from "./clicky-buttons.html";
+import themeCanvas from "./theme-canvas.html";
 
 const server = Bun.serve({
   routes: {
     "/": index,
     "/clicky-buttons": clickyButtons,
+    "/theme-canvas": themeCanvas,
   },
 });
 
